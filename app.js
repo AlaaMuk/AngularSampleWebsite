05cd0579f20971ff60e0f40465e403b92c6c5b88
AlaaMuk <alaa.muk@gmail.com>
2016-03-14 16:26:32 -0400

    Initial

---
+++ app.js
@@ -0,0 +1,22 @@
+// Modules
+(function() {
+    var app = angular.module('store',[]);
+// where store is Application name, and array [] is where the dependencies go.
+
+
+// Controller name should be
+app.controller('StoreController', function (){
+    this.product = gem;
+});
+
+var gem = {
+    name: "Dodecahedron",
+    price: 2.95,
+    description: '...',
+
+}
+
+
+
+
+})();
