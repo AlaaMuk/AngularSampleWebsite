7d7ff1b2cf6377de95b68273454a87e2d7c8fdfb
AlaaMuk <alaa.muk@gmail.com>
2016-03-14 18:14:23 -0400

    added multiple variables

--- app.js
+++ app.js
@@ -6,19 +6,32 @@
 
 // Controller name should be Capitalized
 app.controller('StoreController', function (){
-    this.product = gem;
+    this.products = gems;
 });
 
-var gem = {
+var gems = [
+{
     name: "Dodecahedron",
-    price: 2.95,
+    price: 2,
     description: 'Some gems have hidden qualities beyond their shine...',
     canPurchase: true,
-    soldOut: true,
-
+    soldOut: false,
+},
+{
+    name: "Pentagonal Gem",
+    price: 5.95,
+    description: 'Another description regarding this gem...',
+    canPurchase: true,
+    soldOut: false,
+},
+{
+    name: "AnotherBeautifulGem",
+    price: 2.95,
+    description: 'Ruby Style...',
+    canPurchase: true,
+    soldOut: false,
 }
 
-
-
+]; // CLOSE ARRAYYYYY
 
 })();
